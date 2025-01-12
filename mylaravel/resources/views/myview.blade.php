11cb5245a5fe7a2dac74f421094186a02f1642b3
Wipzin <Auo2548@gmail.com>
2025-01-12 16:55:02 +0700

    Update myview.blade.php

--- mylaravel/resources/views/myview.blade.php
+++ mylaravel/resources/views/myview.blade.php
@@ -1,14 +1,20 @@
-<h1> My view </h1>
+<h1> สูตรคูณ </h1>
 <?php echo $value_id; ?>
 {{ $value_id; }}
 
 <form action = "{{url('mycontroller')}}" method = "post">
   @csrf
-  <input type = "test" name = "myinput">
+  ใส่ตัวเลข: <input type = "test" name = "name">
   <button type ="submit">
     Submit
   </button>
 </form>
-<?php for($i=0;$i<12;$i++){?>
-  <h1>{{$i}}</h1>
-<?php } ?>
+<?php if (isset($_POST["name"]) && is_numeric($_POST["name"])) {
+  $number = intval($_POST["name"]);
+  echo "<h3>สูตรคูณแม่ $number</h3>";
+  for ($i = 1; $i <= 12; $i++) {
+      $result = $i * $number;
+      echo "<p>$number x $i = $result</p>";
+  }
+}
+ ?>
