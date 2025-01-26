ee5b68caf5a8539823cab51b7dacdef3dd1ae9ae
Wipzin <Auo2548@gmail.com>
2025-01-26 15:22:14 +0700

    up

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -3,11 +3,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
 
+Route::get("/login" ,
+    [LoginController::class,'index']);
+Route::get("/register" ,
+    [RegisterController::class,'index']);
+Route::get("/home" ,
+    [HomeController::class,'index']);
+Route::get("/" ,
+    [HomeController::class,'index']);
+    
 Route::get('/', function () {
     return view('layout.default');
 });
-Route::get('/hello', function () {
-    return "<h1>Hello World!</h1>";
+Route::get('/hello/{id?}',
+    function ($val="") {
+    return "<h1>Hello World!$val</h1>";
 });
-Route::get("/mycontroller/{id?}" , [MyController::class,'myfunction']);
-Route::post("/mycontroller/{id?}" , [MyController::class,'myfunction']);
+Route::get("/mycontroller/{id?}" ,
+    [MyController::class,'myfunction']);
+Route::post("/mycontroller/{id?}" ,
+    [MyController::class,'MYFUNCTION']);
