362053a0e9e35ce601693b2c0ae013599d2c5efe
Wipzin <Auo2548@gmail.com>
2025-01-27 11:29:28 +0700

    up

--- mylaravel/resources/views/errors/minimal.blade.php
+++ mylaravel/resources/views/errors/minimal.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <div class="login-page">
         <div style="display: flex; justify-content: center; align-items: center; min-height: 100vh; text-align: center;">
-            
             <div>
                 <h1>@yield('code')</h1>
+                <img src="image/silly.jpg">
                 <p>@yield('message')</p>
             </div>
         </div>
